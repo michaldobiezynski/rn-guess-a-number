56d2bfbd2a577120cd6466ad694cdc615df3b978
michaldobiezynski <dobiezynski@gmail.com>
2020-05-17 07:08:38 +0100

    extracting a card component (presentational component)

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
+import Card from '../components/Card';
+
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>The Game Screen</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title='Reset' onPress={() => {}} />
           <Button title='Confirm' onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -33,14 +35,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center',
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    backgroundColor: 'white',
-    elevation: 5,
-    padding: 20,
-    borderRadius: 10,
   },
   title: {
     fontSize: 20,
