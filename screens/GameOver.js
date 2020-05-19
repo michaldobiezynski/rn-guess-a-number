6a449ab48d14d639994f5062cf2ef0d5a4c2c6db
michaldobiezynski <dobiezynski@gmail.com>
2020-05-19 08:55:34 +0100

    a closer look at the Text component and what I can do with it

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -3,9 +3,10 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import Title from '../components/Title';
+import Colors from '../constants/colors';
 
 const GameOver = (props) => {
-  // let gameOverPicture = require('../assets/original.png');
+  let gameOverPicture = require('../assets/original.png');
 
   return (
     <View style={styles.screen}>
@@ -15,21 +16,36 @@ const GameOver = (props) => {
           fadeDuration={1000}
           resizeMode='cover'
           style={styles.image}
-          // source={gameOverPicture}
-          source={{
-            uri: 'https://ichef.bbci.co.uk/images/ic/1200x675/p01qtgbf.jpg',
-          }}
+          source={gameOverPicture}
+          // source={{
+          //   uri: 'https://ichef.bbci.co.uk/images/ic/1200x675/p01qtgbf.jpg',
+          // }}
           resizeMode='cover'
         />
       </View>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{' '}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{' '}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
+
       <Button title='NEW GAME' onPress={props.onRestart}></Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  resultContainer: {
+    marginHorizontal: 40,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
   screen: {
     flex: 1,
     justifyContent: 'center',
@@ -47,6 +63,10 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     overflow: 'hidden',
     marginVertical: 30,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold',
   },
 });
 
